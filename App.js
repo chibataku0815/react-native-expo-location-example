b4e80dc3611fde93818fc9ac9e12d17b7ca207c1
千葉工 <chibataku0815@gmail.com>
2021-08-01 00:05:09 +0900

    added getLocationPermissions

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
+import * as Permissions from 'expo-permissions';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+
+  const [locationPermission, setLocationPermission] = useState(false)
+
+  useEffect(() => {
+    const getLocationPermissions = async () => {
+      let { status } = await Permissions.askAsync( Permissions.LOCATION )
+      if ( 'granted' === status ) {
+        setLocationPermission( true );
+      }
+    }
+    getLocationPermissions()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {
+        locationPermission ? '位置情報へのアクセス許可されました' : '位置情報へのアクセス許可が拒否されました'
+      }
       <StatusBar style="auto" />
     </View>
   );
