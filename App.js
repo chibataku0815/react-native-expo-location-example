9906adc622ecf60367c9a8e449ba4d443075a6a9
千葉工 <chibataku0815@gmail.com>
2021-08-01 14:23:21 +0900

    Get the coords

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
@@ -6,12 +7,15 @@ import { StyleSheet, View } from 'react-native';
 export default function App() {
 
   const [locationPermission, setLocationPermission] = useState(false)
+  const [isCoords, setCoords] = useState()
 
   useEffect(() => {
     const getLocationPermissions = async () => {
       let { status } = await Permissions.askAsync( Permissions.LOCATION )
       if ( 'granted' === status ) {
-        setLocationPermission( true );
+        await setLocationPermission( true );
+        let { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({})
+        setCoords({ latitude, longitude })
       }
     }
     getLocationPermissions()
@@ -19,9 +23,14 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {
-        locationPermission ? '位置情報へのアクセス許可されました' : '位置情報へのアクセス許可が拒否されました'
-      }
+      <p>
+        {
+          locationPermission ? '位置情報へのアクセス許可されました' : '位置情報へのアクセス許可が拒否されました'
+        }
+      </p>
+      <p>
+        座標 {JSON.stringify(isCoords)}
+      </p>
       <StatusBar style="auto" />
     </View>
   );
